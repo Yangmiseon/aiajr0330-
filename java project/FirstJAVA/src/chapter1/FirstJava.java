4dd0163962f1f1b46679bcb3a96358737cdf2718
seon <didaltjs87@gmail.com>
2020-04-08 18:38:03 +0900

    Create FirstJava.java

---
+++ java project/FirstJAVA/src/chapter1/FirstJava.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+
+		int num;
+		num = 2 + 5;
+		System.out.println("2+5="+num + ", "   + 2+5);
+		System.out.println("2+5="+num  +", "  + (2+5));
+		
+		
+		int a = 10;
+		int b = 5;
+		System.out.println(15); //정수의 출력
+		System.out.println("10+5="+(a+b)); //덧셈 결과 출력
+		System.out.println("1"+"5"); //숫자조합출력
+		System.out.println("15"); //문자열 출력
+		System.out.println( 3*5);//곱셉 결과 출력
+		
+
+	}
+		
+}
+
+
+
+
+
+
