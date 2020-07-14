05f91f7445ed0c86ee97ed00f71b6cffef24b2d4
Yangmiseon <didaltjs87@gmail.com>
2020-07-14 18:23:37 +0900

    openmvc

---
+++ jsp project/OpenProjectMVC/src/member/model/Member.java
@@ -0,0 +1,92 @@
+package member.model;
+
+import java.sql.Date;
+
+public class Member {
+
+	private int idx;
+	private String uid;
+	private String upw;
+	private String uname;
+	private String uphoto;
+	private Date regdate;
+	
+	public Member(int idx, String uid, String upw, String uname, String uphoto, Date regdate) {
+		this.idx = idx;
+		this.uid = uid;
+		this.upw = upw;
+		this.uname = uname;
+		this.uphoto = uphoto;
+		this.regdate = regdate;
+	}
+
+	
+	public Member() {
+	}
+
+	
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getUpw() {
+		return upw;
+	}
+
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUphoto() {
+		return uphoto;
+	}
+
+	public void setUphoto(String uphoto) {
+		this.uphoto = uphoto;
+	}
+
+	public Date getRegdate() {//long타입으로 가져오게되기때문에 변수만드러줌
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	//변수
+	//java.sql.Date >> java.util.Date로 바꿔주는메서드
+	public java.util.Date getToDate(){//부를때>>${member.toDate}
+		
+		return new java.util.Date(regdate.getTime());
+	}
+
+
+	@Override
+	public String toString() {
+		return "Member [idx=" + idx + ", uid=" + uid + ", upw=" + upw + ", uname=" + uname + ", uphoto=" + uphoto
+				+ ", regdate=" + regdate + "]";
+	}
+
+	
+	
+}
